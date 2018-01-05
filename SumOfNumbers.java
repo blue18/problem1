f04cadd6029ac086a7f685d961176d5ba2a17ca6
blue18 <garcb969@icloud.com>
2018-01-04 21:47:26 -0800

    created git

---
+++ SumOfNumbers.java
@@ -0,0 +1,75 @@
+/* Write three functions that compute the sum of the numbers in a 
+   given list using a for-loop, a while-loop, and recursion.*/
+import java.io.*;
+import java.util.*;
+
+public class SumOfNumbers
+{
+	ArrayList<Integer> list = new ArrayList<Integer>();
+
+	public SumOfNumbers(ArrayList<Integer> n) {
+		list.addAll(0, n);
+	}
+
+	// for-loop function
+	int forLoop() {
+		int total = 0;
+
+		for (int i = 0; i < list.size(); i++) {
+			total += list.get(i);
+		}
+
+		return total;
+	}
+
+	// while-loop function
+	int whileLoop() {
+		int total = 0;
+		int i = 0;
+
+		while(list.size() > i) {
+			total += list.get(i);
+			i++;
+		}
+
+		return total;
+	}
+
+	// recursion function
+	int recursion() {
+ 		return recursionHelper(list.get(list.size() - 1));	
+	}
+
+	int recursionHelper(int n) {
+		
+		list.remove(list.size() - 1);
+
+		if (list.isEmpty()) {
+			return n;
+		} else {
+			System.out.print(list.get(list.size() - 1) + "\n");
+			return n + recursionHelper(list.get(list.size() - 1));
+		}
+	}
+
+
+
+   public static void main(String args[]) {
+
+	   	ArrayList<Integer> data = new ArrayList<Integer>();
+	   	data.add(10);
+	   	data.add(12);
+	   	data.add(20);
+	   	SumOfNumbers x = new SumOfNumbers(data);
+	   	System.out.print(x.recursion() + "\n");
+
+   }
+}
+
+
+
+
+
+
+
+
